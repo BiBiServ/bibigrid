51879c85f81443c064f76187c9762570a812a83c
Andreas Lueckner <alueckne@techfak.uni-bielefeld.de>
2014-01-26 18:17:17 +0100

    Added all new instances for the newest API

--- src/de/unibi/cebitec/bibigrid/util/InstanceInformation.java
+++ src/de/unibi/cebitec/bibigrid/util/InstanceInformation.java
@@ -9,24 +9,49 @@ public class InstanceInformation {
     private static final Map<InstanceType, InstanceSpecification> count = new HashMap<>();
 
     static {
-        count.put(InstanceType.T1Micro, new InstanceSpecification(1, 0, false));
-        count.put(InstanceType.M1Small, new InstanceSpecification(1, 1, false));
-        count.put(InstanceType.M1Medium, new InstanceSpecification(1, 1, false));
-        count.put(InstanceType.M1Large, new InstanceSpecification(2, 2, false));
-        count.put(InstanceType.M1Xlarge, new InstanceSpecification(4, 4, false));
+        // General use
         count.put(InstanceType.M3Xlarge, new InstanceSpecification(4, 0, false));
         count.put(InstanceType.M32xlarge, new InstanceSpecification(8, 0, false));
+        
+        count.put(InstanceType.M1Small, new InstanceSpecification(1, 1, false));
+        count.put(InstanceType.M1Medium, new InstanceSpecification(1, 1, false));
+        
+        count.put(InstanceType.M1Large, new InstanceSpecification(2, 2, false));
+        count.put(InstanceType.M1Xlarge, new InstanceSpecification(4, 4, false));
+        
+        
+        //compute optimized
+        count.put(InstanceType.C3Large, new InstanceSpecification(2, 2, false));
+        count.put(InstanceType.C3Xlarge, new InstanceSpecification(4, 2, false));
+        count.put(InstanceType.C32xlarge, new InstanceSpecification(8, 2, false));
+        count.put(InstanceType.C34xlarge, new InstanceSpecification(16, 2, false));
+        count.put(InstanceType.C38xlarge, new InstanceSpecification(32, 2, false));
+        
+        count.put(InstanceType.C1Medium, new InstanceSpecification(2, 1, false));
+        count.put(InstanceType.C1Xlarge, new InstanceSpecification(8, 4, false));
+        count.put(InstanceType.Cc28xlarge, new InstanceSpecification(32, 4, true));
+
+        // GPU 
+        count.put(InstanceType.G22xlarge, new InstanceSpecification(8, 1, true));
+        count.put(InstanceType.Cg14xlarge, new InstanceSpecification(16, 2, true));
+
+        // Memory optimized
         count.put(InstanceType.M2Xlarge, new InstanceSpecification(2, 1, false));
         count.put(InstanceType.M22xlarge, new InstanceSpecification(4, 1, false));
         count.put(InstanceType.M24xlarge, new InstanceSpecification(8, 2, false));
-        count.put(InstanceType.C1Medium, new InstanceSpecification(2, 1, false));
-        count.put(InstanceType.C1Xlarge, new InstanceSpecification(8, 4, false));
-        count.put(InstanceType.Cc14xlarge, new InstanceSpecification(8, 2, true));
-        count.put(InstanceType.Cc28xlarge, new InstanceSpecification(32, 4, true));
-        count.put(InstanceType.Cg14xlarge, new InstanceSpecification(16, 2, true));
+        count.put(InstanceType.Cr18xlarge, new InstanceSpecification(32, 2, true));
+
+        // Storage optimized
         count.put(InstanceType.Hi14xlarge, new InstanceSpecification(16, 2, true));
         count.put(InstanceType.Hs18xlarge, new InstanceSpecification(16, 24, true));
-        count.put(InstanceType.Cr18xlarge, new InstanceSpecification(32, 2, true));
+
+        //  I2 Instances
+        count.put(InstanceType.I2Xlarge, new InstanceSpecification(4, 1, true));
+        count.put(InstanceType.I22xlarge, new InstanceSpecification(8, 2, true));
+        count.put(InstanceType.I24xlarge, new InstanceSpecification(16, 4, true));
+        count.put(InstanceType.I28xlarge, new InstanceSpecification(32, 8, true));
+        // micro
+        count.put(InstanceType.T1Micro, new InstanceSpecification(1, 0, false));
     }
 
     private InstanceInformation() {
