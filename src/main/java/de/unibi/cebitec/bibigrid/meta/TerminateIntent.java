39eb5f7d6f8d59b752d15f11ade76f9c55a8ce98
Jan Krueger <jkrueger@cebitec.uni-bielefeld.de>
2016-08-15 16:07:15 +0200

    fix issues #2, #11 and partial #5
    - add better errorhandling for issue#5 (print full stack trace)
    - abort if initialization of instances failed (and terminate running parts of the cluster)
    - if something failed during initialisation -> some test for 'null' objects are still missing

--- src/main/java/de/unibi/cebitec/bibigrid/meta/TerminateIntent.java
+++ src/main/java/de/unibi/cebitec/bibigrid/meta/TerminateIntent.java
@@ -1,15 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package de.unibi.cebitec.bibigrid.meta;
 
 /**
  *
- * @author jsteiner
+ * @author Johanes Steiner
  */
 public interface TerminateIntent {
-    
+
+    /**
+     * Terminate a cluster.
+     * @return Return true in case of success, false otherwise
+     */
     public boolean terminate();
 }
