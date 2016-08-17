500fd0e2d60af337005fce84152317ce192992c5
Jan Krueger <jkrueger@cebitec.uni-bielefeld.de>
2016-08-17 22:12:28 +0200

    add ConfigurationException

---
+++ src/main/java/de/unibi/cebitec/bibigrid/model/ConfigurationException.java
@@ -0,0 +1,23 @@
+
+package de.unibi.cebitec.bibigrid.model;
+
+/**
+ *
+ * @author  Jan Krueger - jkrueger(at)cebitec.uni-bielefeld.de
+ */
+public class ConfigurationException extends Exception {
+   
+    public ConfigurationException(){
+        super();
+    }
+    
+    public ConfigurationException(String m){
+        super(m);
+    }
+    
+    public ConfigurationException(String m, Throwable t){
+        super(m,t);
+    }
+    
+    
+}
