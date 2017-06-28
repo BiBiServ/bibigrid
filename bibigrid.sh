3bab9201fe949afe2c1f1cd40cf7881cb2f2590e
Jan Krueger <jkrueger@course39.desktop>
2017-06-28 16:03:48 +0200

    fix typo

--- bibigrid.sh
+++ bibigrid.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-if [ ${0} = -bash ] ; then
+
+if [ ${0} = "bash" -o ${0} = "-bash" ] ; then
  alias bibigrid="java -jar `pwd`/`ls target/BiBiGrid-*.jar`"
 else 
- java -jar `dirname $0`/`ls target/BiBiGrid-*.jar` $@
+ java -jar `dirname ${0}`/`ls target/BiBiGrid-*.jar` $@
 fi
 
