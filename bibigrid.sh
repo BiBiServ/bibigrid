47576258ef53d21abb0383b98bc90ae94e77e6a1
Jan Krueger <jkrueger@cebitec.uni-bielefeld.de>
2018-02-06 21:39:44 +0100

    Fix(#60): add different profiles for each cloud provider, rework naming schema ...

--- bibigrid.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-if [ ${0} = "bash" -o ${0} = "-bash" ] ; then
- alias bibigrid="java -jar `pwd`/`ls bibigrid-main/target/BiBiGrid-*.jar`"
-else 
- java -jar `dirname ${0}`/`ls bibigrid-main/target/BiBiGrid-*.jar` $@
-fi
-
