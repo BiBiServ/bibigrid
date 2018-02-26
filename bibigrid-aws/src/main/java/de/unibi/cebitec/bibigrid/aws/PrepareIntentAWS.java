2981c88cf24e527ac9e6a7165c2294c32ff50b27
AstrorEnales <mfriedrichs@techfak.uni-bielefeld.de>
2018-02-26 21:13:21 +0100

    feat(prepare): #67 - Basic provider implementations for prepare intent

--- bibigrid-aws/src/main/java/de/unibi/cebitec/bibigrid/aws/PrepareIntentAWS.java
+++ bibigrid-aws/src/main/java/de/unibi/cebitec/bibigrid/aws/PrepareIntentAWS.java
@@ -1,29 +1,65 @@
 package de.unibi.cebitec.bibigrid.aws;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.model.*;
 import de.unibi.cebitec.bibigrid.core.intents.PrepareIntent;
 import de.unibi.cebitec.bibigrid.core.model.Instance;
 import de.unibi.cebitec.bibigrid.core.model.ProviderModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static de.unibi.cebitec.bibigrid.core.util.VerboseOutputFilter.V;
 
 /**
  * @author mfriedrichs(at)techfak.uni-bielefeld.de
  */
 class PrepareIntentAWS extends PrepareIntent {
+    private static final Logger LOG = LoggerFactory.getLogger(PrepareIntentAWS.class);
+
+    private final AmazonEC2 ec2;
+
     PrepareIntentAWS(ProviderModule providerModule, ConfigurationAWS config) {
         super(providerModule, config);
+        ec2 = IntentUtils.getClient(config);
     }
 
     @Override
     protected boolean stopInstance(Instance instance) {
-        return false;
+        String instanceId = ((InstanceAWS) instance).getInternal().getInstanceId();
+        StopInstancesRequest request = new StopInstancesRequest().withInstanceIds(instanceId);
+        ec2.stopInstances(request);
+        return true;
     }
 
     @Override
     protected void waitForInstanceShutdown(Instance instance) {
-
+        String instanceId = ((InstanceAWS) instance).getInternal().getInstanceId();
+        do {
+            DescribeInstancesRequest request = new DescribeInstancesRequest().withInstanceIds(instanceId);
+            try {
+                DescribeInstancesResult result = ec2.describeInstances(request);
+                InstanceState state = result.getReservations().get(0).getInstances().get(0).getState();
+                LOG.info(V, "Status of instance '{}': {}", instance.getName(), state.getName());
+                if (state.getName().equals(InstanceStateName.Stopped.toString())) {
+                    break;
+                } else {
+                    LOG.info(V, "...");
+                    sleep(10);
+                }
+            } catch (AmazonServiceException e) {
+                LOG.debug("{}", e);
+                sleep(3);
+            }
+        } while (true);
     }
 
     @Override
     protected boolean createImageFromInstance(Instance instance, String imageName) {
-        return false;
+        String instanceId = ((InstanceAWS) instance).getInternal().getInstanceId();
+        CreateImageRequest request = new CreateImageRequest().withName(imageName).withInstanceId(instanceId);
+        // TODO: tag IMAGE_SOURCE_LABEL
+        CreateImageResult result = ec2.createImage(request);
+        return result != null && result.getImageId() != null && result.getImageId().length() > 0;
     }
 }
--- bibigrid-azure/src/main/java/de/unibi/cebitec/bibigrid/azure/PrepareIntentAzure.java
+++ bibigrid-azure/src/main/java/de/unibi/cebitec/bibigrid/azure/PrepareIntentAzure.java
@@ -1,29 +1,62 @@
 package de.unibi.cebitec.bibigrid.azure;
 
+import com.microsoft.azure.management.Azure;
+import com.microsoft.azure.management.compute.PowerState;
+import com.microsoft.azure.management.compute.VirtualMachine;
 import de.unibi.cebitec.bibigrid.core.intents.PrepareIntent;
 import de.unibi.cebitec.bibigrid.core.model.Instance;
 import de.unibi.cebitec.bibigrid.core.model.ProviderModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static de.unibi.cebitec.bibigrid.core.util.VerboseOutputFilter.V;
 
 /**
  * @author mfriedrichs(at)techfak.uni-bielefeld.de
  */
 class PrepareIntentAzure extends PrepareIntent {
+    private static final Logger LOG = LoggerFactory.getLogger(PrepareIntentAzure.class);
+
+    private final Azure compute;
+
     PrepareIntentAzure(ProviderModule providerModule, ConfigurationAzure config) {
         super(providerModule, config);
+        compute = AzureUtils.getComputeService(config);
     }
 
     @Override
     protected boolean stopInstance(Instance instance) {
-        return false;
+        ((InstanceAzure) instance).getInternal().powerOffAsync();
+        return true;
     }
 
     @Override
     protected void waitForInstanceShutdown(Instance instance) {
-
+        VirtualMachine nativeInstance = ((InstanceAzure) instance).getInternal();
+        do {
+            nativeInstance = nativeInstance.refresh();
+            PowerState state = nativeInstance.powerState();
+            LOG.info(V, "Status of instance '{}': {}", instance.getName(), state);
+            if (state == PowerState.STOPPED) {
+                break;
+            } else {
+                LOG.info(V, "...");
+                sleep(10);
+            }
+        } while (true);
     }
 
     @Override
     protected boolean createImageFromInstance(Instance instance, String imageName) {
-        return false;
+        VirtualMachine nativeInstance = ((InstanceAzure) instance).getInternal();
+        nativeInstance.generalize();
+        compute.virtualMachineCustomImages()
+                .define(imageName)
+                .withRegion(config.getRegion())
+                .withNewResourceGroup()
+                .fromVirtualMachine(nativeInstance)
+                .withTag(IMAGE_SOURCE_LABEL, instance.getConfiguration().getImage())
+                .create();
+        return true;
     }
 }
