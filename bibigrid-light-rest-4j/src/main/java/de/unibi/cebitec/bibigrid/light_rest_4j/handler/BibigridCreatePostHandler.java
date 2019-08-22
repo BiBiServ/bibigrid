564e84e680bf705fd57f7abe468b83e53c5ddc47
Tim Niklas Rose <timen.rose@gmail.com>
2019-08-22 16:58:21 +0200

    new project structure to pom relations working

--- bibigrid-light-rest-4j/src/main/java/de/unibi/cebitec/bibigrid/light_rest_4j/handler/BibigridCreatePostHandler.java
+++ bibigrid-light-rest-4j/src/main/java/de/unibi/cebitec/bibigrid/light_rest_4j/handler/BibigridCreatePostHandler.java
@@ -1,17 +1,19 @@
 package de.unibi.cebitec.bibigrid.light_rest_4j.handler;
 
+import com.networknt.body.BodyHandler;
 import com.networknt.handler.LightHttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.HttpString;
 import java.util.HashMap;
 import java.util.Map;
 
-public class BibigridInfoIdPostHandler implements LightHttpHandler {
+public class BibigridCreatePostHandler implements LightHttpHandler {
     
     @Override
     public void handleRequest(HttpServerExchange exchange) throws Exception {
-        exchange.getResponseHeaders().add(new HttpString("Content-Type"), "application/json");
-        exchange.setStatusCode(200);
-        exchange.getResponseSender().send("{\"info\":\"Ich bin noch am hochfahren\",\"log\":\"lorem ipsum lorem ipsum....\"}");
+
+        Map<String, Object> body = (Map<String, Object>)exchange.getAttachment(BodyHandler.REQUEST_BODY);
+
+        exchange.endExchange();
     }
 }
