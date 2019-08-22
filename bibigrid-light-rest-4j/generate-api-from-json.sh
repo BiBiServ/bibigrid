653b98127c42c4b865ee346c9a1c39573eeb12a5
Tim Niklas Rose <timen.rose@gmail.com>
2019-08-22 16:58:21 +0200

    new project structure to pom relations working

--- bibigrid-light-rest-4j/generate-api-from-json.sh
+++ bibigrid-light-rest-4j/generate-api-from-json.sh
@@ -1,6 +1,4 @@
-<<<<<<< cc21bc97ebee526803caf147f57fb4d1ea0d967e
-rm -rf application
-rm -rf .mvn
+# rm -rf .mvn
 rm -rf .gitignore
 rm -rf .build.sh
 rm kubernetes.yml
@@ -8,10 +6,6 @@ rm LICENSE
 rm -rf src
 rm -rf mvnw
 rm -rf mvnw.cmd
-rm -rf target
-rm -f dependency-reduced-pom.xml
-rm -f bibigrid_light_rest_4j.iml
-rm -rf .mvn
-rm -rf docker
+#rm -r pom.xml
 
 java -jar light-codegen/codegen-cli/target/codegen-cli.jar -f openapi -o ./ -m openapi.json -c config.json
