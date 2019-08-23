8cd1291d73d94df86a703b5eff044b64be0dacd9
Tim Niklas Rose <timen.rose@gmail.com>
2019-08-23 10:33:29 +0200

    figured out how to parse request body into model

--- bibigrid-light-rest-4j/src/main/java/de/unibi/cebitec/bibigrid/light_rest_4j/handler/BibigridListPostHandler.java
+++ bibigrid-light-rest-4j/src/main/java/de/unibi/cebitec/bibigrid/light_rest_4j/handler/BibigridListPostHandler.java
@@ -1,7 +1,11 @@
 package de.unibi.cebitec.bibigrid.light_rest_4j.handler;
 
+import com.networknt.body.BodyHandler;
+import com.networknt.config.Config;
 import com.networknt.handler.LightHttpHandler;
+import de.unibi.cebitec.bibigrid.light_rest_4j.model.Access;
 import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Attachable;
 import io.undertow.util.HttpString;
 import java.util.HashMap;
 import java.util.Map;
@@ -10,6 +14,12 @@ public class BibigridListPostHandler implements LightHttpHandler {
     
     @Override
     public void handleRequest(HttpServerExchange exchange) throws Exception {
+
+        Map<String, Object> body = (Map<String, Object>)exchange.getAttachment(BodyHandler.REQUEST_BODY);
+        Access file = Config.getInstance().getMapper().convertValue(body, Access.class);
+
+        System.out.println(file);
+
         exchange.endExchange();
     }
 }
