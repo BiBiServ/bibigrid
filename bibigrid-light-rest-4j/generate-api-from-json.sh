f64063a2e32496534c6bd19d66c3eb7ece707240
Tim Niklas Rose <timen.rose@gmail.com>
2019-08-27 18:57:01 +0200

    tried to implement the validate feature in my handler

--- bibigrid-light-rest-4j/generate-api-from-json.sh
+++ bibigrid-light-rest-4j/generate-api-from-json.sh
@@ -6,6 +6,10 @@ rm LICENSE
 rm -rf src
 rm -rf mvnw
 rm -rf mvnw.cmd
-#rm -r pom.xml
+rm -rf target
+rm -f dependency-reduced-pom.xml
+rm -f bibigrid_light_rest_4j.iml
+rm -rf .mvn
+rm -rf docker
 
 java -jar light-codegen/codegen-cli/target/codegen-cli.jar -f openapi -o ./ -m openapi.json -c config.json
