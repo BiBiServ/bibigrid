6b899d6cef3e21aef93d1360599d1e6976f94610
Tim Niklas Rose <timen.rose@gmail.com>
2019-09-03 12:43:55 +0200

    new api specification that doesnt require Access and ClusterConfiguration Model  in order to avoid having two synonymous models, Configuration and ClusterConfiguration, which have the same aim. Also parsed the json body and tested the string encoding, which now works in validate controller.

--- bibigrid-light-rest-4j/src/main/java/de/unibi/cebitec/bibigrid/light_rest_4j/handler/BibigridValidatePostHandler.java
+++ bibigrid-light-rest-4j/src/main/java/de/unibi/cebitec/bibigrid/light_rest_4j/handler/BibigridValidatePostHandler.java
@@ -1,88 +1,33 @@
 package de.unibi.cebitec.bibigrid.light_rest_4j.handler;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.networknt.body.BodyHandler;
-import com.networknt.config.Config;
-
-
 import com.networknt.handler.LightHttpHandler;
-import de.unibi.cebitec.bibigrid.Provider;
-import de.unibi.cebitec.bibigrid.core.Validator;
-import de.unibi.cebitec.bibigrid.core.model.Client;
-import de.unibi.cebitec.bibigrid.core.model.Configuration;
-import de.unibi.cebitec.bibigrid.core.model.ProviderModule;
-import de.unibi.cebitec.bibigrid.core.model.exceptions.ClientConnectionFailedException;
-import de.unibi.cebitec.bibigrid.core.model.exceptions.ConfigurationException;
-import de.unibi.cebitec.bibigrid.light_rest_4j.model.Access;
-import de.unibi.cebitec.bibigrid.light_rest_4j.model.ClusterConfiguration;
 import io.undertow.server.HttpServerExchange;
+import io.undertow.util.HttpString;
+import org.jose4j.json.internal.json_simple.JSONObject;
+import org.jose4j.json.internal.json_simple.parser.JSONParser;
 
-import java.io.InputStream;
+import java.util.Base64;
+import java.util.HashMap;
 import java.util.Map;
 
-import static de.unibi.cebitec.bibigrid.core.util.ImportantInfoOutputFilter.I;
-
-public class BibigridValidatePostHandler implements LightHttpHandler{
-
+public class BibigridValidatePostHandler implements LightHttpHandler {
+    
     @Override
     public void handleRequest(HttpServerExchange exchange) throws Exception {
 
 
-        Map<String, Object> body = (Map<String, Object>)exchange.getAttachment(BodyHandler.REQUEST_BODY);
+        ObjectMapper mapper = new ObjectMapper();
+        Map s = (Map)exchange.getAttachment(BodyHandler.REQUEST_BODY);
+        String j = mapper.writeValueAsString(s);
+        JSONObject json = (JSONObject)new JSONParser().parse(j);
+        byte[] decodedBytes = Base64.getDecoder().decode( (String) json.get("credentialsFile"));
+        String decodedString = new String(decodedBytes);
 
-        Access file = Config.getInstance().getMapper().convertValue(body.get("access"), Access.class);
-        ClusterConfiguration cfg =  Config.getInstance().getMapper().convertValue(body.get("clusterConfiguration"), ClusterConfiguration.class);
-
-        System.out.println(file);
-        System.out.println(cfg);
-
-
-        ProviderModule module;
-        String providerMode = cfg.getMode();
-        module = Provider.getInstance().getProviderModule(providerMode);
-
-        Validator validator;
-//        try {
-//            validator = module.getCommandLineValidator(commandLine, configurationFile, intentMode);
-//        } catch (ConfigurationException e) {
-//            LOG.error(e.getMessage());
-//            LOG.error(ABORT_WITH_NOTHING_STARTED);
-//            return;
-//        }
-//
-//        if (validator.validate(providerMode)) {
-//            Client client;
-//            try {
-//                client = module.getClient(validator.getConfig());
-//
-//                if (module.getValidateIntent(client, true)) {
-//                    System.out.println("You can now start your cluster.");
-//                } else {
-//                    System.out.println("There were one or more errors. Please adjust your configuration.");
-//                }
-//
-//
-//            } catch (ClientConnectionFailedException e) {
-//                LOG.error(e.getMessage());
-//                LOG.error(ABORT_WITH_NOTHING_STARTED);
-//                return;
-//            }
-//        }
-
-
-
-
-
-
-
-
-
-
-
-
-
+        System.out.println(decodedString);
 
 
         exchange.endExchange();
     }
-
 }
