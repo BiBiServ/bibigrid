fa3839786944c5bd8f283d5fc6f979b042f3d953
Tim Niklas Rose <timen.rose@gmail.com>
2019-09-03 12:43:55 +0200

    new api specification that doesnt require Access and ClusterConfiguration Model  in order to avoid having two synonymous models, Configuration and ClusterConfiguration, which have the same aim. Also parsed the json body and tested the string encoding, which now works in validate controller.

--- bibigrid-light-rest-4j/src/test/java/de/unibi/cebitec/bibigrid/light_rest_4j/handler/BibigridInfoIdPostHandlerTest.java
+++ bibigrid-light-rest-4j/src/test/java/de/unibi/cebitec/bibigrid/light_rest_4j/handler/BibigridInfoIdPostHandlerTest.java
@@ -54,7 +54,7 @@ public class BibigridInfoIdPostHandlerTest {
             throw new ClientException(e);
         }
         final AtomicReference<ClientResponse> reference = new AtomicReference<>();
-        String requestUri = "/bibigrid/info/QaSjZgaPscPKKYUvplnc";
+        String requestUri = "/bibigrid/info/pdjUdTzXBOhJrabhIX";
         String httpMethod = "post";
         try {
             ClientRequest request = new ClientRequest().setPath(requestUri).setMethod(Methods.POST);
