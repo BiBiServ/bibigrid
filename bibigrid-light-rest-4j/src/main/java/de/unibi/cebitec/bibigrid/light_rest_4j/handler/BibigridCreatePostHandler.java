dcacdfb602d20f0d4740154f138e73082ef4868e
Tim Niklas Rose <timen.rose@gmail.com>
2019-09-03 12:43:55 +0200

    new api specification that doesnt require Access and ClusterConfiguration Model  in order to avoid having two synonymous models, Configuration and ClusterConfiguration, which have the same aim. Also parsed the json body and tested the string encoding, which now works in validate controller.

--- bibigrid-light-rest-4j/src/main/java/de/unibi/cebitec/bibigrid/light_rest_4j/handler/BibigridCreatePostHandler.java
+++ bibigrid-light-rest-4j/src/main/java/de/unibi/cebitec/bibigrid/light_rest_4j/handler/BibigridCreatePostHandler.java
@@ -1,10 +1,6 @@
 package de.unibi.cebitec.bibigrid.light_rest_4j.handler;
 
-import com.networknt.body.BodyHandler;
-import com.networknt.config.Config;
 import com.networknt.handler.LightHttpHandler;
-import de.unibi.cebitec.bibigrid.light_rest_4j.model.Access;
-import de.unibi.cebitec.bibigrid.light_rest_4j.model.ClusterConfiguration;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.HttpString;
 import java.util.HashMap;
@@ -14,16 +10,6 @@ public class BibigridCreatePostHandler implements LightHttpHandler {
     
     @Override
     public void handleRequest(HttpServerExchange exchange) throws Exception {
-
-
-        Map<String, Object> body = (Map<String, Object>)exchange.getAttachment(BodyHandler.REQUEST_BODY);
-
-        Access file = Config.getInstance().getMapper().convertValue(body.get("access"), Access.class);
-        ClusterConfiguration cfg =  Config.getInstance().getMapper().convertValue(body.get("clusterConfiguration"), ClusterConfiguration.class);
-
-        System.out.println(file);
-        System.out.println(cfg);
-
         exchange.endExchange();
     }
 }
