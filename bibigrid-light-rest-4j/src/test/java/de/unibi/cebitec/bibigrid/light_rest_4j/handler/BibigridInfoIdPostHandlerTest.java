d1dd10a859643b6f1f215bf93e014cbf9c7d0a77
Tim Niklas Rose <timen.rose@gmail.com>
2019-09-20 14:59:11 +0200

    added workerInstances field to api

--- bibigrid-light-rest-4j/src/test/java/de/unibi/cebitec/bibigrid/light_rest_4j/handler/BibigridInfoIdPostHandlerTest.java
+++ bibigrid-light-rest-4j/src/test/java/de/unibi/cebitec/bibigrid/light_rest_4j/handler/BibigridInfoIdPostHandlerTest.java
@@ -54,7 +54,7 @@ public class BibigridInfoIdPostHandlerTest {
             throw new ClientException(e);
         }
         final AtomicReference<ClientResponse> reference = new AtomicReference<>();
-        String requestUri = "/bibigrid/info/dUraZAgyVdkakbwzTHerGgGrwdgS";
+        String requestUri = "/bibigrid/info/zGhGvoGjHgbiFXYJ";
         String httpMethod = "post";
         try {
             ClientRequest request = new ClientRequest().setPath(requestUri).setMethod(Methods.POST);
