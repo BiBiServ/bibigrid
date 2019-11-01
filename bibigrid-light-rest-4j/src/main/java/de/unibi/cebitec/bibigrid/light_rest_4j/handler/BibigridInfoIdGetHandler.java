c312d5020715476faab21fc66bce40458a3212dd
Tim Niklas Rose <timen.rose@gmail.com>
2019-11-01 15:50:00 +0100

    Implemented /info/id route, added test .log file and excluded it from .gitignore

--- bibigrid-light-rest-4j/src/main/java/de/unibi/cebitec/bibigrid/light_rest_4j/handler/BibigridInfoIdGetHandler.java
+++ bibigrid-light-rest-4j/src/main/java/de/unibi/cebitec/bibigrid/light_rest_4j/handler/BibigridInfoIdGetHandler.java
@@ -3,15 +3,44 @@ package de.unibi.cebitec.bibigrid.light_rest_4j.handler;
 import com.networknt.handler.LightHttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.HttpString;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
 
 public class BibigridInfoIdGetHandler implements LightHttpHandler {
-    
+
     @Override
-    public void handleRequest(HttpServerExchange exchange) throws Exception {
-        exchange.getResponseHeaders().add(new HttpString("Content-Type"), "application/json");
-        exchange.setStatusCode(200);
-        exchange.getResponseSender().send("{\"info\":\"Ich bin noch am hochfahren\",\"log\":\"lorem ipsum lorem ipsum....\"}");
+    public void handleRequest(HttpServerExchange exchange){
+        ServiceProviderConnector serviceProviderConnector = new ServiceProviderConnector();
+        if(serviceProviderConnector.connectToServiceProvider(exchange)){
+            String clusterId = exchange.getQueryParameters().get("id").getFirst();
+            try{
+                FileInputStream fstream = new FileInputStream("../../bibigrid-main/"+clusterId+".log");
+                BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+                String strLine;
+                String lastLine = "";
+                while ((strLine = br.readLine()) != null)
+                {
+                    lastLine = strLine;
+                }
+                fstream.close();
+                exchange.getResponseHeaders().add(new HttpString("Content-Type"), "application/json");
+                exchange.setStatusCode(200);
+                exchange.getResponseSender().send("{\"info\":\"Starting up\",\"log\":\""+lastLine+"\"}");
+
+            } catch (Exception e) {
+                System.err.println("Error: " + e.getMessage());
+                exchange.getResponseHeaders().add(new HttpString("Content-Type"), "application/json");
+                exchange.setStatusCode(200);
+                exchange.getResponseSender().send("{\"error\":\"Unable to find log file.\"}");
+            }
+
+        } else {
+            exchange.getResponseHeaders().add(new HttpString("Content-Type"), "application/json");
+            exchange.setStatusCode(400);
+            exchange.getResponseSender().send("{\"error\":\""+serviceProviderConnector.getError()+"\"}");
+        }
+        exchange.endExchange();
     }
 }
