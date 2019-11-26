4deefa46f8d4cf481c0092c0e8c731108ad9cf79
Tim Niklas Rose <timen.rose@gmail.com>
2019-11-26 21:31:10 +0100

    Refactored ServiceProviderConnector into more methods: - Copied initProviderModule() from StartUp.java to create a module. - Used existing methods of ValidatorOpenstack for loading env vars. To   access the ValidatorOpenstack it had to be made public.

--- bibigrid-openstack/src/main/java/de/unibi/cebitec/bibigrid/openstack/ValidatorOpenstack.java
+++ bibigrid-openstack/src/main/java/de/unibi/cebitec/bibigrid/openstack/ValidatorOpenstack.java
@@ -34,7 +34,7 @@ public final class ValidatorOpenstack extends Validator {
         OS_USERNAME
     }
 
-    ValidatorOpenstack(final Configuration config, final ProviderModule providerModule)
+    public ValidatorOpenstack(final Configuration config, final ProviderModule providerModule)
             throws ConfigurationException {
         super( config, providerModule);
         openstackConfig = (ConfigurationOpenstack) config;
@@ -124,7 +124,7 @@ public final class ValidatorOpenstack extends Validator {
      * Loads environment variables.
      * @return openStackCredentials if parameters given, otherwise null
      */
-    private OpenStackCredentials loadEnvCredentials() {
+    public OpenStackCredentials loadEnvCredentials() {
         Map env =  System.getenv();
         OpenStackCredentials openStackCredentials = new OpenStackCredentials();
 
