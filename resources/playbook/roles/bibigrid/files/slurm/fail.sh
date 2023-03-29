0eaaf0378d599d89cfd6cd2d2eb56977f3fbc940
XaverStiensmeier <xaverstiensmeier@gmx.de>
2023-03-29 15:14:41 +0200

    fixed fail and terminate script

--- resources/playbook/roles/bibigrid/files/slurm/fail.sh
+++ resources/playbook/roles/bibigrid/files/slurm/fail.sh
@@ -13,6 +13,8 @@ log "Fail-Script started"
 # $1 is in slurm node format for example: bibigrid-worker0-cid-[0-1],bibigrid-worker1-cid-0 and needs no converting
 scontrol update NodeName="$1" state=RESUME reason=FailedStartup # no sudo needed cause executed by slurm user
 
+hosts=$(scontrol show hostnames "$1")
+
 # delete servers
 python3 /usr/local/bin/delete_server.py "${hosts}"
 
