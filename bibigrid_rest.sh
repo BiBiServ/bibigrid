7ed1bda15e2df34615629d35571997127ce247fd
XaverStiensmeier <xaverstiensmeier@gmx.de>
2023-08-16 16:54:46 +0200

    Added cpu based worker selection

--- bibigrid_rest.sh
+++ bibigrid_rest.sh
@@ -1,1 +1,8 @@
-uvicorn bibigrid.core.startup_rest:app --reload
+#!/bin/bash
+if [ "$#" -eq  "0" ]
+  then
+     echo "Picked $(expr 2 \* $(nproc --all) + 1) workers (CPU+1)"
+     uvicorn bibigrid.core.startup_rest:app --workers $(expr 2 \* $(nproc --all) + 1)
+else
+     uvicorn bibigrid.core.startup_rest:app --workers #1
+fi
