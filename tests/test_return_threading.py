c9b580f2520960b52af6953be300da3260b7b8cc
XaverStiensmeier <xaverstiensmeier@gmx.de>
2023-10-20 12:09:31 +0200

    updated return threading test

--- tests/test_return_threading.py
+++ tests/test_return_threading.py
@@ -1,17 +1,22 @@
+"""
+Module to test return thread
+"""
 from unittest import TestCase
 
 import bibigrid.models.return_threading as returnThreading
 
 
-def test_method(x):
-    return (42, x)
+def test_method(elem):
+    return 42, elem
 
 
 class TestReturnThread(TestCase):
+    """
+    Class to test return thread
+    """
 
-    def test_ReturnThread(self):
-        return_thread = returnThreading.ReturnThread(target=test_method,
-                                                     args=[42])
+    def test_return_thread(self):
+        return_thread = returnThreading.ReturnThread(target=test_method, args=[42])
         return_thread.start()
         return_value = return_thread.join()
         self.assertTrue(return_value == (42, 42))
