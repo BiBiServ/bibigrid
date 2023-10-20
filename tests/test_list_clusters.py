83d4a2a4321e4d43f007b5f29b6c1d17ea9f64cf
XaverStiensmeier <xaverstiensmeier@gmx.de>
2023-10-20 16:03:30 +0200

    test list clusters updated

--- tests/test_list_clusters.py
+++ tests/test_list_clusters.py
@@ -13,36 +13,45 @@ class TestList(TestCase):
     """
     Class test list
     """
+
     def test_setup(self):
         for identifier in [create.WORKER_IDENTIFIER, create.VPN_WORKER_IDENTIFIER, create.MASTER_IDENTIFIER]:
-            cluster_id = 42
+            cluster_id = "42"
             provider = Mock()
             provider.name = "name"
-            provider.cloud_specification = {"identifier": 21}
+            provider.cloud_specification = {"identifier": "21"}
             cluster_dict = {}
-            server = {"name": identifier(cluster_id=str(cluster_id))}
-            self.assertEqual(str(cluster_id), list_clusters.setup(server, identifier, cluster_dict, provider))
-            self.assertEqual({str(cluster_id): {'worker': [], 'vpngtw': []}}, cluster_dict)
-            self.assertEqual(provider, server["provider"])
+            server = {"name": identifier(cluster_id=cluster_id)}
+            list_clusters.setup(cluster_dict, str(cluster_id), server, provider)
+
+            self.assertEqual({cluster_id: {'workers': [], 'vpngtws': []}}, cluster_dict)
+            self.assertEqual(provider.NAME, server["provider"])
+            self.assertEqual("21", server["cloud_specification"])
 
     def test_setup_already(self):
         for identifier in [create.WORKER_IDENTIFIER, create.VPN_WORKER_IDENTIFIER, create.MASTER_IDENTIFIER]:
-            cluster_id = 42
-            test_provider = Mock()
-            test_provider.name = "name"
-            cluster_dict = {str(cluster_id): {'worker': ["some"], 'vpngtw': ["some"]}}
-            server = {"name": identifier(cluster_id=str(cluster_id))}
-            self.assertEqual(str(cluster_id), list_clusters.setup(server, identifier, cluster_dict, test_provider))
-            self.assertEqual({str(cluster_id): {'worker': ["some"], 'vpngtw': ["some"]}}, cluster_dict)
-            self.assertEqual(test_provider, server["provider"])
+            cluster_id = "42"
+            provider = Mock()
+            provider.name = "name"
+            provider.cloud_specification = {"identifier": "21"}
+            cluster_dict = {cluster_id: {'workers': ["some"], 'vpngtws': ["some"]}}
+            server = {"name": identifier(cluster_id=cluster_id)}
+            list_clusters.setup(cluster_dict, cluster_id, server, provider)
+
+            self.assertEqual({cluster_id: {'workers': ["some"], 'vpngtws': ["some"]}}, cluster_dict)
+            self.assertEqual(provider.NAME, server["provider"])
+            self.assertEqual("21", server["cloud_specification"])
 
     def test_dict_clusters(self):
-        cluster_id = 42
-        expected = {str(cluster_id): {'workers': [{'name': f'bibigrid-worker-{str(cluster_id)}', 'provider': 'Mock'}],
-                                      'vpngtws': [{'name': f'bibigrid-vpngtw-{str(cluster_id)}', 'provider': 'Mock'}],
-                                      'master': {'name': f'bibigrid-master-{str(cluster_id)}', 'provider': 'Mock'}}}
+        cluster_id = "42"
+        expected = {cluster_id: {
+            'workers': [{'cloud_specification': '21', 'name': f'bibigrid-worker-{cluster_id}-0', 'provider': 'Mock'}],
+            'vpngtws': [{'cloud_specification': '21', 'name': f'bibigrid-vpngtw-{cluster_id}-0', 'provider': 'Mock'}],
+            'master': {'cloud_specification': '21', 'name': f'bibigrid-master-{cluster_id}', 'provider': 'Mock'}}}
         provider = Mock()
-        provider.list_servers.return_value = [{'name': identifier(cluster_id=str(cluster_id))} for identifier in
-                                              [create.WORKER_IDENTIFIER, create.VPN_WORKER_IDENTIFIER,
-                                               create.MASTER_IDENTIFIER]]
+        provider.NAME = "Mock"
+        provider.cloud_specification = {"identifier": "21"}
+        provider.list_servers.return_value = [{'name': identifier(cluster_id=cluster_id) + "-0"} for identifier in
+                                              [create.WORKER_IDENTIFIER, create.VPN_WORKER_IDENTIFIER]] + [
+                                                 {'name': create.MASTER_IDENTIFIER(cluster_id=cluster_id)}]
         self.assertEqual(expected, list_clusters.dict_clusters([provider], startup.LOG))
