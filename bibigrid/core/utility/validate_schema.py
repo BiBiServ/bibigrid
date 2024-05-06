ea2d47fa4ae26bf07a2cc799a10c1d9588283257
XaverStiensmeier <xaverstiensmeier@gmx.de>
2024-05-06 15:58:53 +0200

    updated validate_configuration.py in order to provide schema validation. Moved cloud_identifier setting even closer to program start in order to be able to log better when performing other actions than create.

---
+++ bibigrid/core/utility/validate_schema.py
@@ -0,0 +1,58 @@
+"""
+Handles the schema validation for BiBiGrid's configuration yml.
+"""
+
+from schema import Schema, Optional, Or, SchemaError
+
+# Define the schema for the configuration file
+master_schema = Schema(
+    {'infrastructure': str, 'cloud': str, 'sshUser': str, Or('subnet', 'network'): str, 'cloud_identifier': str,
+     Optional('sshPublicKeyFiles'): [str], Optional('sshTimeout'): int,
+     Optional('cloudScheduling'): {Optional('sshTimeout'): int}, Optional('autoMount'): bool,
+     Optional('masterMounts'): [str], Optional('nfsShares'): [str],
+     Optional('userRoles'): [{'hosts': [str], 'roles': [{'name': str, Optional('tags'): [str]}]}],
+     Optional('localFS'): bool, Optional('localDNSlookup'): bool, Optional('slurm'): bool,
+     Optional('slurmConf'): {Optional('db'): str, Optional('db_user'): str, Optional('db_password'): str,
+                             Optional('munge_key'): str, Optional('elastic_scheduling'): {Optional('SuspendTime'): int,
+                                                                                          Optional(
+                                                                                              'ResumeTimeout'): int,
+                                                                                          Optional('TreeWidth'): int}},
+     Optional('zabbix'): bool, Optional('nfs'): bool, Optional('ide'): bool, Optional('useMasterAsCompute'): bool,
+     Optional('useMasterWithPublicIP'): bool, Optional('waitForServices'): [str],
+     Optional('gateway'): {'ip': str, 'portFunction': str}, Optional('fallbackOnOtherImage'): bool,
+     Optional('localDNSLookup'): bool, Optional('features'): [str], 'workerInstances': [
+        {'type': str, 'image': str, Optional('count'): int, Optional('onDemand'): bool, Optional('partitions'): [str],
+         Optional('features'): [str]}],
+     'masterInstance': {'type': str, 'image': str, Optional('partitions'): [str], Optional('features'): [str]},
+     Optional('vpngtw'): {'type': str, 'image': str}})
+
+other_schema = Schema(
+    {'infrastructure': str, 'cloud': str, 'sshUser': str, Or('subnet', 'network'): str, 'cloud_identifier': str,
+     Optional('waitForServices'): [str], Optional('features'): [str], 'workerInstances': [
+        {'type': str, 'image': str, Optional('count'): int, Optional('onDemand'): bool, Optional('partitions'): [str],
+         Optional('features'): [str]}], 'vpngtw': {'type': str, 'image': str}})
+
+
+def validate_configurations(configurations, log):
+    configuration = None
+    try:
+        configuration = configurations[0]
+        if configuration.get("region") or configuration.get("availabilityZone"):
+            log.warning(
+                "Keys region and availabilityZone are deprecated! Check will return False if you use one of them."
+                "Just remove them. They are no longer required.")
+        master_schema.validate(configuration)
+        log.debug(f"Master configuration '{configuration['cloud_identifier']}' valid.")
+        for configuration in configurations[1:]:
+            if configuration.get("region") or configuration.get("availabilityZone"):
+                log.warning(
+                    "Keys region and availabilityZone are deprecated! Check will return False if you use one of them."
+                    "Just remove them. They are no longer required.")
+            other_schema.validate(configuration)
+            log.debug(f"Configuration '{configuration['cloud_identifier']}' valid.")
+        return True
+    except SchemaError as err:
+        log.warning(
+            f"Configuration '{configuration.get('cloud_identifier', 'No identifier found')}' invalid. See error: "
+            f"{err}.")
+        return False
--- documentation/markdown/features/configuration.md
+++ documentation/markdown/features/configuration.md
@@ -101,7 +101,9 @@ userRoles: # see ansible_hosts for all options
     - hosts: 
         - "master"
       roles: # roles placed in resources/playbook/roles_user
-        - name: "resistance_nextflow" 
+        - name: "resistance_nextflow"
+          tags:
+            - resistance_nextflow
       # varsFiles: # (optional)
       #  - file1
 ```
@@ -120,8 +122,8 @@ If `True`, master will store DNS information for his workers. Default is `False`
 If `False`, the cluster will start without the job scheduling system slurm.
 This is relevant to the fewest. Default is `True`.
 
-##### SlurmConf (optional)
-`SlurmConf` contains variable fields in the `slurm.conf`. The most common use is to increase the `SuspendTime` 
+##### slurmConf (optional)
+`slurmConf` contains variable fields in the `slurm.conf`. The most common use is to increase the `SuspendTime` 
 and the `ResumeTimeout` like:
 
 ```yaml
@@ -166,13 +168,6 @@ If `False`, master will no longer help workers to process jobs. Default is `True
 
 If `False`, master will not be created with an attached floating ip. Default is `True`.
 
-#### waitForServices (optional):
-
-Expects a list of services to wait for.
-This is required if your provider has any post-launch services interfering with the package manager. If not set,
-seemingly random errors can occur when the service interrupts ansible's execution. Services are
-listed on [de.NBI Wiki](https://cloud.denbi.de/wiki/) at `Computer Center Specific` (not yet).
-
 #### gateway (optional)
 In order to save valuable floating ips, BiBiGrid can also make use of a gateway to create the cluster.
 For more information on how to set up a gateway, how gateways work and why they save floating ips please continue reading [here](https://cloud.denbi.de/wiki/Tutorials/SaveFloatingIPs/).
@@ -192,6 +187,13 @@ gateway:
 Using gateway also automatically sets [useMasterWithPublicIp](#usemasterwithpublicip-optional) to `False`.
 
 ### Local
+
+#### waitForServices (optional):
+
+Expects a list of services to wait for.
+This is required if your provider has any post-launch services interfering with the package manager. If not set,
+seemingly random errors can occur when the service interrupts ansible's execution. Services are
+listed on [de.NBI Wiki](https://cloud.denbi.de/wiki/) at `Computer Center Specific` (not yet).
 
 #### infrastructure (required)
 
