347fcd6ae043049149de27fb3a0356a4a3f8c070
Manuel Koesters <17874544+MKoesters@users.noreply.github.com>
2025-07-03 10:45:06 +0200

    Add build system
    - use uv for dependency management
    - add bibigrid entrypoint
    - use click for command line parsing

--- bibigrid/core/startup.py
+++ bibigrid/core/startup.py
@@ -1,13 +1,14 @@
 """
 Contains main method. Interprets command line, sets logging and starts corresponding action.
 """
+import click
 import logging
 import math
 import os
 import sys
 import time
 import traceback
-
+from types import SimpleNamespace
 import yaml
 
 from bibigrid.core.actions import check, create, ide, list_clusters, terminate, update, version
@@ -121,29 +122,100 @@ def run_action(args, configurations, config_path):
     return exit_state
 
 
-def main():
+# def main():
+#     """
+#     Interprets command line, sets logger, reads configuration and runs selected action. Then exits.
+#     @return:
+#     """
+#     logging.basicConfig(format=LOGGER_FORMAT)
+#     # LOG.addHandler(logging.StreamHandler())  # stdout
+#     LOG.addHandler(logging.FileHandler("bibigrid.log"))  # file
+#     args = command_line_interpreter.interpret_command_line()
+#     set_logger_verbosity(args.verbose)
+
+#     configurations = configuration_handler.read_configuration(LOG, args.config_input)
+#     if not configurations:
+#         sys.exit(1)
+#     configurations = configuration_handler.merge_configurations(
+#         user_config=configurations,
+#         default_config_path=args.default_config_input,
+#         enforced_config_path=args.enforced_config_input,
+#         log=LOG
+#     )
+#     if configurations:
+#         sys.exit(run_action(args, configurations, args.config_input))
+
+
+# if __name__ == "__main__":
+#     main()
+
+@click.command()
+@click.option('-h', '--help', is_flag=True, help='Show this message and exit.')
+@click.option('-v', '--verbose', is_flag=True, help='Enable verbose output.')
+@click.option('-d', '--debug', is_flag=True, help='Enable debug mode.')
+@click.option('-i', '--config-input', type=click.Path(), help='Path to user configuration.')
+@click.option('-di', '--default-config-input', type=click.Path(), help='Path to default configuration.')
+@click.option('-ei', '--enforced-config-input', type=click.Path(), help='Path to enforced configuration.')
+@click.option('-cid', '--cluster-id', help='Cluster ID.')
+@click.option('-V', '--version', is_flag=True, help='Show version and exit.')
+@click.option('-t', '--terminate', is_flag=True, help='Terminate cluster.')
+@click.option('-c', '--create', is_flag=True, help='Create cluster.')
+@click.option('-l', '--list', is_flag=True, help='List clusters.')
+@click.option('-ch', '--check', is_flag=True, help='Check cluster.')
+@click.option('-ide', '--ide', is_flag=True, help='IDE mode.')
+@click.option('-u', '--update', is_flag=True, help='Update cluster.')
+def main(help, verbose, debug, config_input, default_config_input, enforced_config_input, cluster_id,
+         version, terminate, create, list, check, ide, update):
     """
     Interprets command line, sets logger, reads configuration and runs selected action. Then exits.
-    @return:
     """
     logging.basicConfig(format=LOGGER_FORMAT)
-    # LOG.addHandler(logging.StreamHandler())  # stdout
-    LOG.addHandler(logging.FileHandler("bibigrid.log"))  # file
-    args = command_line_interpreter.interpret_command_line()
-    set_logger_verbosity(args.verbose)
+    LOG.addHandler(logging.FileHandler("bibigrid.log"))
 
-    configurations = configuration_handler.read_configuration(LOG, args.config_input)
+    # Enforce that exactly one action is selected
+    actions = [version, terminate, create, list, check, ide, update]
+    if sum(actions) != 1:
+        # Click's fail() prints usage and exits with error
+        ctx = click.get_current_context()
+        ctx.fail("One (and only one) of the arguments -V/--version -t/--terminate -c/--create -l/--list -ch/--check -ide/--ide -u/--update is required")
+
+    set_logger_verbosity(verbose)
+
+    # Replace with your actual configuration handler
+    configurations = configuration_handler.read_configuration(LOG, config_input)
     if not configurations:
         sys.exit(1)
+
+    if default_config_input is None:
+        default_config_input = ""
+    if enforced_config_input is None:
+        enforced_config_input = ""
     configurations = configuration_handler.merge_configurations(
         user_config=configurations,
-        default_config_path=args.default_config_input,
-        enforced_config_path=args.enforced_config_input,
+        default_config_path=default_config_input,
+        enforced_config_path=enforced_config_input,
         log=LOG
     )
-    if configurations:
-        sys.exit(run_action(args, configurations, args.config_input))
 
+    args = SimpleNamespace(
+        config_input=config_input,
+        default_config_input=default_config_input,
+        enforced_config_input=enforced_config_input,
+        cluster_id=cluster_id,
+        version=version,
+        terminate=terminate,
+        create=create,
+        list=list,
+        check=check,
+        ide=ide,
+        update=update,
+        verbose=verbose,
+        debug=debug
+    )
+
+    if configurations:
+        # You may want to pass the action as a string instead of the entire args object
+        sys.exit(run_action(args, configurations, config_input))
 
 if __name__ == "__main__":
     main()
